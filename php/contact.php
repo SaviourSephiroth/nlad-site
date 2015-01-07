8e1e69fac52495d98a811469707a6adf524ee4da
Kieran Lad <kieranlad@gmail.com>
2015-01-07 17:42:54 +0000

    Started on the contact form

--- php/contact.php
+++ php/contact.php
@@ -1,13 +1,37 @@
 <?php
-	echo '<h2>CONTACT US</h2>
-				
-				<h5>E-mail us:</h5>
-				<p><mailto>enquiries@naginlad.com</mailto></p>
+echo '
+	<h2>CONTACT US</h2>	
+	<h3>Interested in Studying with NLAC?</h3>
+	<p>Fill in the form below to register your interest with NLAC. All information submitted will be held in accordance with our data protection policy which can be found <a href="dataprotection"> here</a></p>
+	<div class="form-holder">
+	<form>
+		<div class="label">Title:</div>
+		<select class="answer">
+			<option>Mr</option>
+			<option>Mrs</option>
+			<option>Ms</option>
+			<option>Miss</option>
+			<option>Dr</option>
+			<option>Other</option>
+		</select>
+		<div class="label">First Name: </div>
+		<div class="answer"> <input></input></div>
+		<div class="label">Surname: </div>
+		<div class="answer"> <input></input></div>
 
-				<h5>Call us:</h5>
-				<p></p>
+		<div class="label">By clicking submit you are agreeing to the Terms and Conditions set out by NLAC which can be read <a href="tandc">here</a> </div>
 
-				<h5>Find us on Facebook:</h5>
-				<p></p>';
+		<input type="submit" value="Submit">
+	</form>
+	</div>
 
+	<h5>E-mail us:</h5>
+	<p><mailto>enquiries@naginlad.com</mailto></p>
+
+	<h5>Call us:</h5>
+	<p></p>
+
+	<h5>Find us on Facebook:</h5>
+	<p></p>
+';
 ?>
--- php/courses.php
+++ php/courses.php
@@ -14,14 +14,29 @@
 					<li>Review the online classes as often as you like</li>
 					<li>Online videos covering all ACCA approved content for all 16 ACCA papers</li>
 				</ul>
+				<p id="learnMore">
+					<a href="contact.php">Learn more</a>
+				</p>
 			</div>
 			<div class=\'course-body\' id=\'classroom-live-body\'>
 				<ul>
 					<li> </li>
 					<li> </li>
 				</ul>
+				<p id="learnMore">
+					<a href="contact.php">Learn more</a>
+				</p>
 			</div>
 		</div>
+
+		<div id=\'course-overview\'>
+			<h3>Course Overview</h3>
+			<h4>Online Classroom:</h4>
+			<iframe class=\'indent\' width="560" height="315" src="//www.youtube.com/embed/5vrwpkVokwo" frameborder="0" allowfullscreen></iframe>
+			<h4>Online Classroom Live:</h4>
+			<iframe class=\'indent\' width="560" height="315" src="//www.youtube.com/embed/HxYmnRBUeZM" frameborder="0" allowfullscreen></iframe>
+		</div>
+ 
 		<div class=\'min-requirements\'>
 			<p>All courses require students to have a computer with the below minimum specifications:</p>
 			<ul>
